74c47a2ff15fe3e6da640a9d2edb7d2d6cf56bf4
AmandaYi <891177434@qq.com>
2020-08-08 00:25:08 +0800

    解决不能验证的问题

--- src/use/ui.js
+++ src/use/ui.js
@@ -2,6 +2,12 @@ import Vue from "vue"
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import Avue from '@smallwei/avue';
+import '@smallwei/avue/lib/index.css';
+
+//如果使用字典需要赋值axios为全局
+import axios from 'axios';
+window.axios=axios
 
 Vue.use(ElementUI)
-
+Vue.use(Avue);
--- src/views/user/add.vue
+++ src/views/user/add.vue
@@ -1,3 +1,46 @@
 <template>
-    <div>add</div>
+    <div>
+        <avue-form :option="option" v-model="form" ref="user">
+            <template slot="menuForm">
+            <el-button type="primary" @click="ok">提 交</el-button>
+            <el-button  @click="rollback">清 空</el-button>
+        </template>
+        </avue-form>
+    </div>
 </template>
+<script>
+export default {
+    data(){
+        return {
+            form:{
+                username:""
+            },
+            option:{
+                column:[{
+                        label:'姓名',
+                        prop:'username',
+                        rules: [{
+                            required: true,
+                            message: "请输入姓名",
+                            trigger: "blur"
+                        }]
+                }]
+            }
+
+        }
+    },
+    methods:{
+        ok(){
+        this.$refs.user.validate((vaild,done)=>{
+          if(vaild){
+           console.log(vaild)
+            setTimeout(()=>{
+              done()
+            },2000)
+          }
+        })
+        }
+    }
+    
+}
+</script>
